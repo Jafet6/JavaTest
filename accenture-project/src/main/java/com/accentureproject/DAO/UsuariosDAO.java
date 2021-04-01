19645e0102ca2bc24d72b831a72ec4f51d4475b9
Jafet Henrique <jafet.h.fagundes@accenture.com>
2021-04-01 15:51:50 -0300

    Usuario DAO feito

--- accenture-project/src/main/java/com/accentureproject/DAO/UsuariosDAO.java
+++ accenture-project/src/main/java/com/accentureproject/DAO/UsuariosDAO.java
@@ -1,5 +1,39 @@
 package com.accentureproject.DAO;
 
-public class UsuariosDAO {
+import com.accentureproject.model.Usuarios;
 
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class UsuariosDAO {
+    private EntityManager em;
+
+    public UsuariosDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public Usuarios criarUsuario(Usuarios usuario) {
+//        em.getTransaction().begin();
+        this.em.persist(usuario);
+        return usuario;
+    }
+
+    public void deletarUsuario(Usuarios usuario) {
+        usuario = em.merge(usuario);
+        this.em.remove(usuario);
+    }
+
+    public Usuarios alterarUsuario(Usuarios usuario) {
+        this.em.merge(usuario);
+        return usuario;
+    }
+
+    public Usuarios buscarPorId(Long id) {
+        return this.em.find(Usuarios.class, id);
+    }
+
+    public List<Usuarios> buscarTodos() {
+        String jpql = "SELECT p FROM Usuarios p";
+        return em.createQuery(jpql, Usuarios.class).getResultList();
+    }
 }
